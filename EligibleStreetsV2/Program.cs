95eaf592a94495ce8bdb0a20de1aa5b759af50b7
Michael Lau <michael.lau@stockport.gov.uk>
2022-03-10 08:56:04 +0000

    chore(program): Add logic to handle additional fields

--- EligibleStreetsV2/Program.cs
+++ EligibleStreetsV2/Program.cs
@@ -10,39 +10,43 @@ namespace EligibleStreetsApp
     {
         static void Main(string[] args)
         {
-            //Grab file 
             string filePath = @"C:\Code\parking-enforcement-service\src\Data\EligibleStreets\EligibleStreets2021_Mike.json";
-            //Grab each line 
             List<string> lines = File.ReadLines(filePath).ToList();
-
             List<string> newData = new();
 
             foreach (string line in lines)
             {
                 if (line.Contains("House Numbers"))
                 {
-                    var rawHouseNumberData = line.Replace("\"House Numbers\": \" ", "").Replace(" \",", "");
-                    var numbers = rawHouseNumberData
-                        .Substring(rawHouseNumberData.IndexOf(':') + 1)
-                        .Replace(@"""", "")
-                        .Replace(@"\", "")
-                        .Replace(@"n", "")
-                        .Split(',');
+                    ModifyHouseRow(newData, line);
+                }
+                else if (line.Contains("Zone (if applicable)"))
+                {
+                    ModifyZoneRow(newData, line);
+                }
+                else if (line.Contains("FIELD2"))
+                {
 
-                    StringBuilder newLine = new StringBuilder("    \"HouseNumbers\": [ ");
+                }
+                else if (line.Contains("Entitlement"))
+                {
+                    ModifyEntitlement(newData, line);
+                }
+                else if (line.Contains("POSTCODE"))
+                {
+                    var rawData = line.Replace("\"POSTCODE\": \"", "");
 
-                    for (int i = 0; i < numbers.Length; i++)
+                    if (rawData.Trim().StartsWith("\\t"))
                     {
-                        if (i.Equals(numbers.Length - 1))
-                        {
-                            GenerateFormatFoLastNumber(numbers, newLine, i);
-                        }
-                        else
-                        {
-                            GenerateFormatForNumberExceptLastNumber(numbers, newLine, i);
-                        }
+                        var rawPostCode = rawData.Trim().Remove(0, 3);
+                        var newString = $"    \"POSTCODE\": \"{rawPostCode.Trim()}";
+
+                        newData.Add(newString);
                     }
-                    newData.Add(newLine.ToString());
+                    else
+                    {
+                        newData.Add(line);
+                    }
                 }
                 else
                 {
@@ -51,10 +55,53 @@ namespace EligibleStreetsApp
             }
 
             string newFilePath = @$"C:\Code\parking-enforcement-service\src\Data\EligibleStreets\EligibleStreets{DateTime.Now:yyyyMMdd}.json";
-            //Produce new json 
             File.WriteAllLines(newFilePath, newData);
             Console.WriteLine("Created!");
             Console.ReadLine();
+        }
+
+        private static void ModifyEntitlement(List<string> newData, string line)
+        {
+            var rawEntitlementData = line.Replace("\"Entitlement\": \"", "");
+            var rawEntitlementOnStreet = rawEntitlementData.Trim().Replace("On Street \",", "On Street\",");
+            var newString = $"    \"Entitlement\": \"{rawEntitlementOnStreet.Trim()}";
+
+            newData.Add(newString);
+        }
+
+        private static void ModifyHouseRow(List<string> newData, string line)
+        {
+            var rawHouseNumberData = line.Replace("\"House Numbers\": \" ", "").Replace(" \",", "");
+            var numbers = rawHouseNumberData
+                .Substring(rawHouseNumberData.IndexOf(':') + 1)
+                .Replace(@"""", "")
+                .Replace(@"\", "")
+                .Replace(@"n", "")
+                .Split(',');
+
+            StringBuilder newLine = new StringBuilder("    \"HouseNumbers\": [ ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i.Equals(numbers.Length - 1))
+                {
+                    GenerateFormatFoLastNumber(numbers, newLine, i);
+                }
+                else
+                {
+                    GenerateFormatForNumberExceptLastNumber(numbers, newLine, i);
+                }
+            }
+            newData.Add(newLine.ToString());
+        }
+
+        private static void ModifyZoneRow(List<string> newData, string line)
+        {
+            var rawZoneData = line.Replace("\"Zone (if applicable)\": \"", "");
+
+            var newString = $"    \"Zone\": \"{rawZoneData.Trim()}";
+
+            newData.Add(newString);
         }
 
         private static void GenerateFormatForNumberExceptLastNumber(string[] numbers, StringBuilder newLine, int i)
